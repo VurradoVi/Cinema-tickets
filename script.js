5495e4fe92e01a3c81379e68d4fc26264a164efc
Vlad <02kvg03@mail.ru>
2024-06-21 19:44:06 +0300

    completed the block booking tickets

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const blocks = document.querySelectorAll('.schedule__block')
 const primarybtn = document.querySelectorAll('.primarybtn')
 const closeSchedule = document.getElementById('closeSchedule')
 const place = document.getElementById('place-grid')
-let clickCount = 0;
+
 
 btnScroll.addEventListener('click', function (event) {
     event.preventDefault()
@@ -51,7 +51,7 @@ closeSchedule.addEventListener('click', function () {
     document.getElementById('schedule-modal').classList.remove('open')
     place.innerHTML = ''
     document.getElementById('place-info').innerHTML = ''
-    clickCount = 0
+    count = 0
 })
 
 openTrailer.addEventListener('click', function () {
@@ -72,32 +72,56 @@ window.addEventListener('keydown', function (event) {
 });
 
 function site() {
-
+    let total = 300
+    let count = 0
     for (let i = 1; i < 11; i++) {
         for (let j = 1; j <= 14; j++) {
             let paragraph = document.createElement('p')
             paragraph.classList.add('place-site')
             paragraph.textContent = `${j}`;
+            paragraph.dataset.row = i;
+            paragraph.dataset.seat = j;
             paragraph.addEventListener('click', function () {
+                let infoElement = document.getElementById('place-info');
+                if (!this.classList.contains('selected')) {
+                    count++
                     this.style.backgroundColor = 'white'
                     this.style.color = 'black'
                     this.style.border = '1px solid blue'
                     this.style.transform = 'scale(1.3)'
                     this.style.lineHeight = '15px'
+                    this.classList.add('selected');
                     let spanElement = document.createElement('span');
-                    let infoElement = document.getElementById('place-info');
-                    spanElement.textContent = `${i} ряд, ${j} место`
+                    spanElement.innerHTML = `${this.dataset.row} ряд, ${this.dataset.seat} место`;
                     infoElement.appendChild(spanElement);
-                //  else {
-                //     this.style.backgroundColor = ''
-                //     this.style.color = ''
-                //     this.style.border = ''
-                //     this.style.transform = ''
-                //     this.style.lineHeight = ''
-                //     infoElement.removeChild(spanElement);
-                //     clickCount = 0
-                // }
-            })
+                    let x = document.createElement('h6')
+                    x.textContent = 'x'
+                    x.classList.add('x-btn')
+                    x.addEventListener('click', function () {
+                        paragraph.style.backgroundColor = '';
+                        paragraph.style.color = '';
+                        paragraph.style.border = '';
+                        paragraph.style.transform = '';
+                        paragraph.style.lineHeight = '';
+                        paragraph.classList.remove('selected');
+                        infoElement.removeChild(spanElement);
+                        count--
+                        document.getElementById('price').innerHTML = `${count} билет(а): ${total*count}р`
+                    })
+                    spanElement.appendChild(x) //кнопка чтобы убрать билет
+                } else {
+                    this.style.backgroundColor = '';
+                    this.style.color = '';
+                    this.style.border = '';
+                    this.style.transform = '';
+                    this.style.lineHeight = '';
+                    this.classList.remove('selected');
+                    infoElement.removeChild(infoElement.lastChild);
+                    count--
+                    document.getElementById('price').innerHTML = `${count} билет(а): ${total*count}р`
+                }
+                document.getElementById('price').innerHTML = `${count} билет(а): ${total*count}р`
+            });
             place.appendChild(paragraph)
             if (j === 10 || j === 11) {
                 paragraph.style.visibility = 'hidden'
@@ -105,4 +129,6 @@ function site() {
         }
     }
 }
+
+
 
